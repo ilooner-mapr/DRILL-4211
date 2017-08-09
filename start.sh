511f479735d4fb818c2869ba05c6f58cc6957f31
Timothy Farkas <timothyfarkas@apache.org>
2017-08-09 10:52:35 -0700

    - Populated database tables for test

--- start.sh
+++ start.sh
@@ -14,5 +14,24 @@ docker run -d --rm --name postgres -p 5432:5432 mapr/postgresdrill4211
 # Populate postgres
 
 waitForPort 5432
+
+## Create cat table
+
 docker exec -i -t postgres psql -U postgres -c 'create table cat (categoryguid text, categoryname text);'
+docker exec -i -t postgres psql -U postgres -c "insert into cat (categoryguid , categoryname) values ('id1', 'restaurants');"
+docker exec -i -t postgres psql -U postgres -c "insert into cat (categoryguid , categoryname) values ('id2', 'Coffee Shops');"
+
+## Create trx table
+
+docker exec -i -t postgres psql -U postgres -c 'create table trx (categoryguid text, trxid text);'
+docker exec -i -t postgres psql -U postgres -c "insert into trx (categoryguid , trxid) values ('id1', 'tid1');"
+docker exec -i -t postgres psql -U postgres -c "insert into trx (categoryguid , trxid) values ('id1', 'tid2');"
+docker exec -i -t postgres psql -U postgres -c "insert into trx (categoryguid , trxid) values ('id2', 'tid3');"
+docker exec -i -t postgres psql -U postgres -c "insert into trx (categoryguid , trxid) values ('id2', 'tid4');"
+
+## Create w1 table
+
+docker exec -i -t postgres psql -U postgres -c 'create table w1 (categoryparentguid text, categoryname text);'
+docker exec -i -t postgres psql -U postgres -c "insert into w1 (categoryparentguid , categoryname) values ('id1', 'food&Dining');"
+docker exec -i -t postgres psql -U postgres -c "insert into w1 (categoryparentguid , categoryname) values ('id2', 'food&Dining');"
 
